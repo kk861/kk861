d40d1929b0fc284ec5a49c64e37091aee205fb23
kk861 <55840730+kk861@users.noreply.github.com>
2019-11-03 12:26:39 +0800

    Create java1101.java

---
+++ java1101.java
@@ -0,0 +1,131 @@
+import javafx.application.Application;
+public class java1101{
+// public static void main(String[] args){
+	
+	// printInfo();
+// }
+// public static void printInfo(){
+	// int a;
+	// int b;
+	// System.out.print(a+b);
+// }
+
+
+// }
+
+// public static double add(double a,double b){
+	// double c=a+b;
+	// return c;
+// }
+// public static void main(String[] args){
+	// int a=1;
+	// int b=2;
+	// double c=add(a+b);
+	// System.out.print(r);
+// }
+
+
+// public static void main(String[] args){
+	// int sum=0;
+	// for(int j=l;j<=5;j++){
+		// sum=sum+factor(j);
+	// }
+	// Syetem.out.println(sum);
+	
+// }
+// public static int factor(int n){
+	// int r=1;
+	// for(int i=1;i<=n;i++)
+	// {
+		// r=r*i;
+	// }
+	// return r;
+// }
+// public static void main(String[] args){
+   
+	// int x=sum(num);
+	// System.out.println(sum(9527));
+// }
+// public static int sum(int num){
+	// if(num<1){
+		// return num;
+	// }
+	// return num%10+sum(num/10);
+// }
+// public static void print(int num){
+	// if(num>9){
+		// print(num/10);
+	// }
+	// System.out.println(num%10);
+// }
+// public static void hanoi(int n,int from,int to){
+	// if(n==1){
+		// System.out.printf("from %d to %d%n",from,to);
+	// }else{
+		// int other=3-from-to;
+		// hanoi(n-1,from,other);
+		// System.out.printf("from %d to %d%n",from,to);
+		// hanoi(n-1,other,to);
+	// }//否则 JavaFX 应用程序类必须扩展javafx.application.Application
+// }
+// public static void main(String[] args){
+	// int[] arr={1,2,3,4,5};
+	// printArray(arr);
+// }
+// public static void printArray(int[] arr){
+	// int t=0;//int max=Integer.MIN_VALUE
+	// for(int i=0;i<arr.length;i++){
+		// if(arr[i]>arr[i+1]){
+			// t=arr[i];
+			// arr[i]=arr[i+1];
+			// arr[i+1]=t;
+		// }
+	// }
+	// System.out.println(arr[arr.length]);
+// }
+
+// public static void main(String[] args){
+	// int[] arr={1,2,3,4,5,5};
+	// printArray(arr);
+// }
+// public static void printArray(int[] arr){
+	// int num=0;
+	// int sum=0;
+	// for(int i=0;i<arr.length;i++){
+		// sum=sum+arr[i];
+	// }
+	// num=sum/arr.length;
+	// System.out.println(num);
+// }
+		// public static void main(String[] args){
+			// int[] arr={1,2,3,4,5,5};
+			// printArray(arr);
+		// }
+		// public static void printArray(int[] arr){
+			// int a=5;
+			// for(i=0;i<arr.length;i++){
+			// if(arr[i]==a){
+				// System.out.println(i);
+			// }
+		// }
+			// return -1;
+
+public static void main(String[] args){
+System.err.println("error print");
+
+}
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
